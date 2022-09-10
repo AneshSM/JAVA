91a52f927253fd2a026afdf9d402201d1bcbb255
Anesh Soamanath Majalikar <aneshsm204@gmail.com>
2022-09-10 14:14:22 +0530

    patterns

--- Pattern/StarPattern.java
+++ Pattern/StarPattern.java
@@ -4,44 +4,51 @@ import java.util.Scanner;
 public class StarPattern{
     
     static Scanner sc=new Scanner(System.in);
+
     public static void rightAngled(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-    public static void rightAngledMirr(int n){
-        for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
-                System.out.print(" ");
-            }
-            for(int j=0;j<i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+    
     public static void BrightAngled(int n){
-        for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-    }   
-    public static void BrightAngledMirr(int n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+    } 
+
+    
+    public static void rightAngledMirr(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<n-i+1;j++){
                 System.out.print(" ");
             }
-            for(int j=n;j>i;j--){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+  
+    public static void BrightAngledMirr(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            for(int j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+   
+    
     public static void Triangle(int n){
         for(int i=0;i<n;i++){
             for(int j=n;j>i;j--){
@@ -133,10 +140,11 @@ public class StarPattern{
         // rightAngled(n);
         // BrightAngled(n);
         // rightAngledMirr(n);
-        // BrightAngledMirr(n);
-        //Triangle(n);
-        // n=n/2;
-        // Diamond(n);
-        
+        BrightAngledMirr(n);
+        // Triangle(n);
+        //TriangleMirr(n);
+        // // n=n/2;
+        // Diamond(n); 
+        //Butterfly(n);
     }
 }
